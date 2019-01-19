662781b982eb1457908a8fcb075c2a217f23a800
Stas Vladyka <stavladyk@gmail.com>
2019-01-19 15:00:33 +0300

    Added new configuration to StandardRegexConstructor (null and empty values)

--- src/main/java/by/tc/task01/dao/util/regex_constructor/StandardRegExConstructorImpl.java
+++ src/main/java/by/tc/task01/dao/util/regex_constructor/StandardRegExConstructorImpl.java
@@ -1,8 +1,10 @@
 package by.tc.task01.dao.util.regex_constructor;
 
+import java.util.Iterator;
+import java.util.Map;
+
 import by.tc.task01.entity.criteria.Criteria;
 
-import java.util.Map;
 
 /**
  * Created by Vladyka Stas
@@ -11,21 +13,37 @@ import java.util.Map;
 public class StandardRegExConstructorImpl implements RegExConstructor {
     public <E> String constructRegEx(Criteria<E> criteria) {
         StringBuilder regex = new StringBuilder();
-        int size = criteria.getCriteria().size();
 
-        //main regex template construction
+        // 1) Excluding element with null\empty values
+        Iterator<Map.Entry<E, Object>> iter = criteria.getCriteria().entrySet().iterator();
+        while (iter.hasNext()) {
+            Map.Entry<E, Object> pair = iter.next();
+            if (pair.getValue() == null || pair.getValue().equals("")) {
+                if (criteria.getCriteria().size() != 1) {
+                    iter.remove();
+                } else break;
+            }
+        }
+        int size = criteria.getCriteria().size();
+        // 2) main regex template construction
         //i don't need cycle here, but it is was easier to write code through "foreach" construction
         for (Map.Entry<E, Object> pair :
                 criteria.getCriteria().entrySet()) {
             //appType
             String appType = pair.getKey().getClass().getSimpleName();
-            //app characteristic value
+            //app characteristic type: CAPACITY, HEIGHT and so on...
             String attribute = pair.getKey().toString();
             // if there is only one criteria for searching
             if (size == 1) {
+                //attribute value is undefined
+                if (pair.getValue() == null || (pair.getValue().equals(""))) {
+                    return regex.append(appType + ".*$").toString();
+                }
+                //attribute value is defined
                 regex.append(appType + " :.+?" + attribute + "=" + pair.getValue() + "[,;].+");
                 return regex.toString();
             }
+
             // if there are several criterias for searching:
             // regex main template construction
             else {
@@ -33,7 +51,8 @@ public class StandardRegExConstructorImpl implements RegExConstructor {
                 break;
             }
         }
-        // repeated part of regex construction
+
+        // 3) repeated part of regex construction
         StringBuilder repeatedPart = new StringBuilder("((");
         int counter = 1;
         for (Map.Entry<E, Object> pair :
@@ -46,10 +65,10 @@ public class StandardRegExConstructorImpl implements RegExConstructor {
             }
             counter++;
         }
-        //final regular creation
-        //adding repeated part of regex
+        // 4) final regular creation
+            //adding repeated part of regex
         regex.append(repeatedPart + "{" + (size - 1) + "}");
-        //deleting from the end of repeated part "?)"
+            //deleting from the end of repeated part "?)"
         regex.append(repeatedPart.substring(1, repeatedPart.length() - 2));
         return regex.toString();
     }
--- src/main/java/by/tc/task01/main/Main.java
+++ src/main/java/by/tc/task01/main/Main.java
@@ -22,14 +22,12 @@ public class Main {
         ServiceFactory factory = ServiceFactory.getInstance();
         ApplianceService service = factory.getApplianceService();
 
-        //////////////////////////////////////////////////////////////////
+        ////////////////////////////////////////////////////////////////
 
         Criteria<Oven> criteriaOven = new Criteria<Oven>();
         criteriaOven.add(Oven.CAPACITY, 32);
         logger.info("Criteria object 'Oven' is created");
-
         appliances = service.find(criteriaOven);
-
         logger.info("Printing objects with specified criteria");
         PrintApplianceInfo.print(appliances, criteriaOven);
         logger.info("Printing objects with specified criteria is done\n");
@@ -40,9 +38,7 @@ public class Main {
         criteriaOven.add(Oven.HEIGHT, 45);
         criteriaOven.add(Oven.DEPTH, 60);
         logger.info("Criteria object 'Oven' is created");
-
         appliances = service.find(criteriaOven);
-
         logger.info("Printing objects with specified criteria");
         PrintApplianceInfo.print(appliances, criteriaOven);
         logger.info("Printing objects with specified criteria is finished\n");
@@ -54,23 +50,47 @@ public class Main {
         criteriaTabletPC.add(TabletPC.DISPLAY_INCHES, 14);
         criteriaTabletPC.add(TabletPC.MEMORY_ROM, 8000);
         logger.info("Criteria object 'TabletPC' is created");
-
         appliances = service.find(criteriaTabletPC);
-
         logger.info("Printing objects with specified criteria");
         PrintApplianceInfo.print(appliances, criteriaTabletPC);
         logger.info("Printing objects with specified criteria is done\n");
 
 
-        Criteria<VacuumCleaner> vacuumCriteria = new Criteria<VacuumCleaner>();
-        vacuumCriteria.add(VacuumCleaner.WAND_TYPE, "all-in-one");
-        logger.info("Criteria object 'VacuumCleaner' is created");
-
-        appliances = service.find(vacuumCriteria);
-
-        logger.info("Printing objects with specified criteria");
-        PrintApplianceInfo.print(appliances, vacuumCriteria);
-        logger.info("Printing objects with specified criteria is done\n");
-
+//        Criteria<VacuumCleaner> vacuumCriteria = new Criteria<VacuumCleaner>();
+//        vacuumCriteria.add(VacuumCleaner.WAND_TYPE, "all-in-one");
+//        logger.info("Criteria object 'VacuumCleaner' is created");
+//        appliances = service.find(vacuumCriteria);
+//        logger.info("Printing objects with specified criteria");
+//        PrintApplianceInfo.print(appliances, vacuumCriteria);
+//        logger.info("Printing objects with specified criteria is done\n");
+//
+//        ////////////////////////////////////////////////////////
+//        criteriaOven = new Criteria<>();
+//        criteriaOven.add(Oven.HEIGHT, "");
+//        logger.info("Criteria object 'Oven' is created");
+//        appliances = service.find(criteriaOven);
+//        logger.info("Printing objects with specified criteria");
+//        PrintApplianceInfo.print(appliances, criteriaOven);
+//        logger.info("Printing objects with specified criteria is finished\n");
+//
+//        criteriaOven = new Criteria<>();
+//        criteriaOven.add(Oven.HEIGHT, null);
+//        criteriaOven.add(Oven.DEPTH, 60);
+//        logger.info("Criteria object 'Oven' is created");
+//        appliances = service.find(criteriaOven);
+//        logger.info("Printing objects with specified criteria");
+//        PrintApplianceInfo.print(appliances, criteriaOven);
+//        logger.info("Printing objects with specified criteria is finished\n");
+//
+//        criteriaOven = new Criteria<>();
+//        criteriaOven.add(Oven.HEIGHT, "");
+//        criteriaOven.add(Oven.DEPTH, 60);
+//        criteriaOven.add(Oven.CAPACITY, "");
+//        criteriaOven.add(Oven.POWER_CONSUMPTION,1000);
+//        logger.info("Criteria object 'Oven' is created");
+//        appliances = service.find(criteriaOven);
+//        logger.info("Printing objects with specified criteria");
+//        PrintApplianceInfo.print(appliances, criteriaOven);
+//        logger.info("Printing objects with specified criteria is finished\n");
     }
 }
